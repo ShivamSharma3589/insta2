04f7212bc990223f0280244bf9857fb8479d0b37
ShivamSharma3589 <85331338+ShivamSharma3589@users.noreply.github.com>
2021-08-26 14:16:02 +0530

    update

--- main.js
+++ main.js
@@ -61,121 +61,121 @@ function storyCreate(value) {
 // post data goes here 
 const postData = [
     {
-        imageIcon : 'icons/user.jpg',
-        name : 'pandatboyy',
-        postImage : 'posts/pandat.jpg',
-        likes : '10,952,764 likes',
-        caption : 'Just setting up my instagram 🙂😎.',
-        views : 'View all 44,980 comments',
-        postTime : '15 May 2021'
+        imageIcon: 'icons/user.jpg',
+        name: 'pandatboyy',
+        postImage: 'posts/pandat.jpg',
+        likes: '10,952,764 likes',
+        caption: 'Just setting up my instagram 🙂😎.',
+        views: 'View all 44,980 comments',
+        postTime: '15 May 2021'
     },
     {
-        imageIcon : 'stories/naveen.jpg',
-        name : 'here_is_naveen_',
-        postImage : 'posts/naveen.jpg',
-        likes : '9,952,764 likes',
-        caption : 'Thinking about <strong>errors</strong> & <strong>bugs</strong>🤔.',
-        views : 'View all 4,993 comments',
-        postTime : '8 August 2021'
+        imageIcon: 'stories/naveen.jpg',
+        name: 'here_is_naveen_',
+        postImage: 'posts/naveen.jpg',
+        likes: '9,952,764 likes',
+        caption: 'Thinking about <strong>errors</strong> & <strong>bugs</strong>🤔.',
+        views: 'View all 4,993 comments',
+        postTime: '8 August 2021'
     },
     {
-        imageIcon : 'stories/ian.jpg',
-        name : 'iansomerhalder',
-        postImage : 'posts/ian.jpg',
-        likes : '52,764 likes',
-        caption : 'I know you are obsessed with me 😘.',
-        views : 'View all 4,520 comments',
-        postTime : '8 December 2021'
+        imageIcon: 'stories/ian.jpg',
+        name: 'iansomerhalder',
+        postImage: 'posts/ian.jpg',
+        likes: '52,764 likes',
+        caption: 'I know you are obsessed with me 😘.',
+        views: 'View all 4,520 comments',
+        postTime: '8 December 2021'
     },
     {
-        imageIcon : 'stories/joseph.jpg',
-        name : 'therealjoseph',
-        postImage : 'posts/joseph.jpg',
-        likes : '23,064 likes',
-        caption : "Yoy've heard of me?...Fantastic 😈",
-        views : 'View all 3,128 comments',
-        postTime : '16 May 2021'
+        imageIcon: 'stories/joseph.jpg',
+        name: 'therealjoseph',
+        postImage: 'posts/joseph.jpg',
+        likes: '23,064 likes',
+        caption: "Yoy've heard of me?...Fantastic 😈",
+        views: 'View all 3,128 comments',
+        postTime: '16 May 2021'
     },
     {
-        imageIcon : 'stories/rdj.jpg',
-        name : 'robertdowneyjr',
-        postImage : 'posts/rdj.jpg',
-        likes : '12,345,764 likes',
-        caption : 'I Am... Iron Man 😉😎.',
-        views : 'View all 7,328 comments',
-        postTime : '4 April 2021'
+        imageIcon: 'stories/rdj.jpg',
+        name: 'robertdowneyjr',
+        postImage: 'posts/rdj.jpg',
+        likes: '12,345,764 likes',
+        caption: 'I Am... Iron Man 😉😎.',
+        views: 'View all 7,328 comments',
+        postTime: '4 April 2021'
     },
     {
-        imageIcon : 'stories/chris.jpg',
-        name : 'chrishemsworth',
-        postImage : 'posts/chris.jpg',
-        likes : '1,232,764 likes',
-        caption : 'I am thor god of thunder ⚡⚡⚡.',
-        views : 'View all 1,068 comments',
-        postTime : '11 August 2021'
+        imageIcon: 'stories/chris.jpg',
+        name: 'chrishemsworth',
+        postImage: 'posts/chris.jpg',
+        likes: '1,232,764 likes',
+        caption: 'I am thor god of thunder ⚡⚡⚡.',
+        views: 'View all 1,068 comments',
+        postTime: '11 August 2021'
     },
     {
-        imageIcon : 'stories/paul.jpg',
-        name : 'paulwesley',
-        postImage : 'posts/paul.jpg',
-        likes : '92,764 likes',
-        caption : 'To beat the villain 🦹‍♂️. you have to be the better villain 😈.',
-        views : 'View all 12,068 comments',
-        postTime : '23 July 2021'
+        imageIcon: 'stories/paul.jpg',
+        name: 'paulwesley',
+        postImage: 'posts/paul.jpg',
+        likes: '92,764 likes',
+        caption: 'To beat the villain 🦹‍♂️. you have to be the better villain 😈.',
+        views: 'View all 12,068 comments',
+        postTime: '23 July 2021'
     },
     {
-        imageIcon : 'stories/tom.jpg',
-        name : 'tom_ellis_',
-        postImage : 'posts/tom.jpg',
-        likes : '9,212,764 likes',
-        caption : 'I am a Devil of my Word 😈',
-        views : 'View all 71,328 comments',
-        postTime : '17 November 2021'
+        imageIcon: 'stories/tom.jpg',
+        name: 'tom_ellis_',
+        postImage: 'posts/tom.jpg',
+        likes: '9,212,764 likes',
+        caption: 'I am a Devil of my Word 😈',
+        views: 'View all 71,328 comments',
+        postTime: '17 November 2021'
     },
     {
-        imageIcon : 'stories/marvel.jpg',
-        name : 'marvelstudios',
-        postImage : 'posts/marvel.jpg',
-        likes : '99,764 likes',
-        caption : 'Let the fun begin',
-        views : 'View all 31,028 comments',
-        postTime : '1 May 2021'
+        imageIcon: 'stories/marvel.jpg',
+        name: 'marvelstudios',
+        postImage: 'posts/marvel.jpg',
+        likes: '99,764 likes',
+        caption: 'Let the fun begin',
+        views: 'View all 31,028 comments',
+        postTime: '1 May 2021'
     },
     {
-        imageIcon : 'stories/netflix.jpg',
-        name : 'Netflix',
-        postImage : 'posts/netflix.jpg',
-        likes : '120,564 likes',
-        caption : 'Lucifer season 6 is out September 10 on Netflix worldwide.',
-        views : 'View all 10,328 comments',
-        postTime : '10 September 2021'
+        imageIcon: 'stories/netflix.jpg',
+        name: 'Netflix',
+        postImage: 'posts/netflix.jpg',
+        likes: '120,564 likes',
+        caption: 'Lucifer season 6 is out September 10 on Netflix worldwide.',
+        views: 'View all 10,328 comments',
+        postTime: '10 September 2021'
     },
     {
-        imageIcon : 'stories/amazon.jpg',
-        name : 'amazonprime',
-        postImage : 'posts/the boys.jpg',
-        likes : '9,782,764 likes',
-        caption : "A stranger is just a friend you ain't met yet.",
-        views : 'View all 1,397 comments',
-        postTime : '13 November 2021'
+        imageIcon: 'stories/amazon.jpg',
+        name: 'amazonprime',
+        postImage: 'posts/the boys.jpg',
+        likes: '9,782,764 likes',
+        caption: "A stranger is just a friend you ain't met yet.",
+        views: 'View all 1,397 comments',
+        postTime: '13 November 2021'
     },
     {
-        imageIcon : 'stories/marvel.jpg',
-        name : 'marvelstudios',
-        postImage : 'posts/marvel2.jpg',
-        likes : '12,764 likes',
-        caption : 'Legends never die.',
-        views : 'View all 99,068 comments',
-        postTime : '3 september 2021'
+        imageIcon: 'stories/marvel.jpg',
+        name: 'marvelstudios',
+        postImage: 'posts/marvel2.jpg',
+        likes: '12,764 likes',
+        caption: 'Legends never die.',
+        views: 'View all 99,068 comments',
+        postTime: '3 september 2021'
     },
     {
-        imageIcon : 'stories/amazon.jpg',
-        name : 'amazonprime',
-        postImage : 'posts/the boys2.jpg',
-        likes : '12,645,764 likes',
-        caption : "The bad guys don't get punished.",
-        views : 'View all 97,028 comments',
-        postTime : '21 October 2021'
+        imageIcon: 'stories/amazon.jpg',
+        name: 'amazonprime',
+        postImage: 'posts/the boys2.jpg',
+        likes: '12,645,764 likes',
+        caption: "The bad guys don't get punished.",
+        views: 'View all 97,028 comments',
+        postTime: '21 October 2021'
     },
 ];
 
@@ -198,6 +198,13 @@ function postCreate(value) {
                           <div class="post-image">
                               <i class="fa fa-heart white-heart"></i>
                               <img src="${postData[value].postImage}">
+                              <div class="collection">
+                                    <div>
+                                        <img src="${postData[value].postImage}">
+                                        <div>Saved</div>
+                                    </div>
+                                    <div>Save to Collection</div>
+                              </div>
                           </div>
                           <div class="post-icons">
                               <div>
@@ -206,7 +213,7 @@ function postCreate(value) {
                                   <img src="icons/forward.png">
                               </div>
                               <div>
-                                  <img src="icons/save-thin.png">
+                                  <img class="save-post" src="icons/save-thin.png">
                               </div>
                           </div>
                           <div class="post-liked">${postData[value].likes}</div>
@@ -226,14 +233,17 @@ function postCreate(value) {
                               </div>
                           </div>
                           <div class="post-time">${postData[value].postTime}</div>`;
-    
-    posts.appendChild(postItem);  
+
+    posts.appendChild(postItem);
 }
 
 // global variables declared here 
 let heart = document.getElementsByClassName('heart');
 let postImage = document.getElementsByClassName('post-image');
 let whiteHeart = document.getElementsByClassName('white-heart');
+let savePost = document.getElementsByClassName('save-post');
+let isPostSaved = [];
+let collection = document.getElementsByClassName('collection');
 
 // window onload function goes here 
 window.onload = () => {
@@ -248,7 +258,7 @@ window.onload = () => {
     for (let i = 0; i < postLength; i++) {
         postCreate(i);
         // function to like the post 
-        heart[i].addEventListener('click',()=>{
+        heart[i].addEventListener('click', () => {
             if (heart[i].style.filter == "none") {
                 heart[i].src = 'icons/heart-thin.png';
                 heart[i].style.filter = 'invert(100%)';
@@ -257,7 +267,9 @@ window.onload = () => {
                 heart[i].style.filter = 'none';
             }
         });
-        postImage[i].addEventListener('dblclick',()=>{
+
+        // function to like the post by using double click 
+        postImage[i].addEventListener('dblclick', () => {
             whiteHeart[i].style.display = 'flex';
             whiteHeart[i].style.transform = 'scale(1)';
             heart[i].src = 'icons/red-heart.png';
@@ -265,6 +277,22 @@ window.onload = () => {
             setTimeout(() => {
                 whiteHeart[i].style.display = 'none';
             }, 800);
+        });
+
+        // save post option 
+        isPostSaved.push(false);
+        savePost[i].addEventListener('click', () => {
+            if (isPostSaved[i]) {
+                savePost[i].src = 'icons/save-thin.png';
+                isPostSaved[i] = false;
+            } else {
+                savePost[i].src = 'icons/save-fill.png';
+                collection[i].style.display = 'flex';
+                setTimeout(() => {
+                    collection[i].style.display = 'none'
+                }, 1000);
+                isPostSaved[i] = true;
+            }
         });
     }
 };
